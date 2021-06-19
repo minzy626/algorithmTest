49d58e49153ca4aebea82f319fe9e6ddda17def7
minzy626 <mjalswl626@gmail.com>
2021-06-19 19:22:00 +0900

    Create K번째수.js

---
+++ K번째수.js
@@ -0,0 +1,10 @@
+function solution(array, commands) {
+    let answer = [];
+    
+    let tempArray=0;
+    
+    for(let command of commands){
+        answer.push(array.slice(command[0]-1,command[1]).sort((a,b)=>a-b)[command[2]-1])
+    }
+    return answer;
+}
